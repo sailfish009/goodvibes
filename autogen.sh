ee991ae6e70bc322f61ce9ae40d971e5ccdcbdcd
Arnaud Rebillout <elboulangero@gmail.com>
2017-02-07 10:02:55 +0700

    build: fix trailing whitespaces in autogen
    Hard to believe that in 2017 I still struggle to do a single
    copy/paste... Is it me or is it vim ?

--- autogen.sh
+++ autogen.sh
@@ -7,12 +7,12 @@ set -e
 echo "Running $0..."
 
 # Enable pre-commit hook (taken verbatim from systemd)
-if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ]; then 
-        # This part is allowed to fail 
-        cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \ 
-        chmod +x .git/hooks/pre-commit && \ 
-        echo "Activated pre-commit hook." || : 
-fi 
+if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ]; then
+        # This part is allowed to fail
+        cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
+        chmod +x .git/hooks/pre-commit && \
+        echo "Activated pre-commit hook." || :
+fi
 
 # Autoreconf
 autoreconf --force --install --verbose

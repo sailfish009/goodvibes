3b9788c47ba10fb86ff33c5cdac7ce3a7f117a29
Arnaud Rebillout <elboulangero@gmail.com>
2017-02-07 08:48:35 +0700

    build: enable defualt git pre-commit hook

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+
 # Run this before running ./configure
+
+set -e
 
 echo "Running $0..."
 
-autoreconf --force --install --verbose || exit $?
+# Enable pre-commit hook (taken verbatim from systemd)
+if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ]; then 
+        # This part is allowed to fail 
+        cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \ 
+        chmod +x .git/hooks/pre-commit && \ 
+        echo "Activated pre-commit hook." || : 
+fi 
+
+# Autoreconf
+autoreconf --force --install --verbose
 
 echo "Done, please type './configure' to continue."
 

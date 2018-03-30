a0e62d455cae1d46eff98610343c42ca98cdbeba
Arnaud Rebillout <elboulangero@gmail.com>
2018-03-30 18:52:31 +0700

    scripts: re-write the translators script

--- scripts/translators.sh
+++ scripts/translators.sh
@@ -2,25 +2,87 @@
 
 # Print the list of translators
 
+LCS=()
+LANGS=()
+NAMES=()
+EMAILS=()
+
 fail() {
     echo >&2 "$@"
     exit 1
 }
 
-[ -d "po" ] || fail "'po' directory not found. Please run this script from the src root."
+usage() {
+    echo "$0 <code/doc>"
+    exit 1
+}
 
-for file in po/*.po; do
-    lang=$(basename $file | cut -d'.' -f1)
-    line=$(grep '^"Last-Translator' $file)
-
+get_translators() {
     # xargs here is used to trim whitespaces
     # https://stackoverflow.com/a/12973694/776208
-    name=$(echo $line | cut -d':' -f2- | cut -d'<' -f1 | xargs)
-    email=$(echo $line | cut -d'<' -f2- | cut -d'>' -f1 | xargs)
 
-    # Normal output for console
-    #echo "$name <$email> ($lang)"
+    for file in po/*.po; do
+	lc=$(basename $file | cut -d'.' -f1)
 
+	line=$(grep '^"Language-Team' $file)
+	lang=$(echo $line | cut -d':' -f2- | cut -d'<' -f1 | xargs)
+
+	line=$(grep '^"Last-Translator' $file)
+	name=$(echo $line | cut -d':' -f2- | cut -d'<' -f1 | xargs)
+	email=$(echo $line | cut -d'<' -f2- | cut -d'>' -f1 | xargs)
+
+	LCS+=("${lc:?}")
+	LANGS+=("${lang:?}")
+	NAMES+=("${name:?}")
+	EMAILS+=("${email:?}")
+
+	#echo "$name <$email> ($lc) ($lang)"
+    done
+}
+
+output_code() {
     # C style output, to be pasted directly in the code
-    echo "	\"$name <$email> ($lang)\n\" \\"
-done
+    local len=${#NAMES[@]}
+    for ((i=0; i<$len; ++i)); do
+	local lc=${LCS[$i]}
+	local lang=${LANGS[$i]}
+	local name=${NAMES[$i]}
+	local email=${EMAILS[$i]}
+	[ $i -lt $(expr $len - 1) ] && \
+	    echo -e "\t\"$name <$email> - $lang ($lc)\\\n\" \\" || \
+	    echo -e "\t\"$name <$email> - $lang ($lc)\";"
+    done
+}
+
+output_doc() {
+    # rst output
+    local len=${#NAMES[@]}
+    for ((i=0; i<$len; ++i)); do
+	local lc=${LCS[$i]}
+	local lang=${LANGS[$i]}
+	local name=${NAMES[$i]}
+	local email=${EMAILS[$i]}
+	echo " * $name - $lang ($lc)"
+    done
+}
+
+[ -d "po" ] || fail "'po' directory not found."
+[ $# -eq 1 ] || usage
+
+get_translators
+
+case $1 in
+    code)
+	echo "-------- 8< --------"
+	output_code
+	echo "-------- >8 --------"
+	;;
+    doc)
+	echo "-------- 8< --------"
+	output_doc
+	echo "-------- >8 --------"
+	;;
+    *)
+	usage
+	;;
+esac

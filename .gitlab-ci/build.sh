e67dd655b725ba0c1515182685ad4b2c23462b7d
Arnaud Rebillout <elboulangero@gmail.com>
2018-05-12 16:23:38 +0700

    gitlab-ci: fix CPPFLAGS in build script

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -6,14 +6,14 @@ set -u
 # no-deprecated-declarations is needed as long as we use GtkStatusIcon
 # no-cast-function-type is needed for GFunc casts, typically:
 #     g_list_foreach(list, (GFunc) g_object_unref, NULL);
-export CPPFLAGS='\
+export CPPFLAGS="\
  -std=gnu99 \
  -Wall \
  -Wextra \
  -Wshadow \
  -Werror \
  -Wno-deprecated-declarations \
- -Wno-cast-function-type'
+ -Wno-cast-function-type"
 
 ./autogen.sh
 ./configure --enable-all

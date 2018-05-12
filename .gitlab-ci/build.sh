5cc732a590e2a1add54c409e886d0e0ac692a6fd
Arnaud Rebillout <elboulangero@gmail.com>
2018-05-12 16:19:27 +0700

    gitlab-ci: ignore cast-function-type warnings, because of GFunc casts

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -3,7 +3,17 @@
 set -e
 set -u
 
-export CPPFLAGS='-std=gnu99 -Wall -Wextra -Wshadow -Werror -Wno-deprecated-declarations'
+# no-deprecated-declarations is needed as long as we use GtkStatusIcon
+# no-cast-function-type is needed for GFunc casts, typically:
+#     g_list_foreach(list, (GFunc) g_object_unref, NULL);
+export CPPFLAGS='\
+ -std=gnu99 \
+ -Wall \
+ -Wextra \
+ -Wshadow \
+ -Werror \
+ -Wno-deprecated-declarations \
+ -Wno-cast-function-type'
 
 ./autogen.sh
 ./configure --enable-all

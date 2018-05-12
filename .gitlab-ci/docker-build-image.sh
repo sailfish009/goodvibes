95f79d271841231896821e115f7104271e38ee8b
Arnaud Rebillout <elboulangero@gmail.com>
2018-05-12 17:06:54 +0700

    gitlab-ci: add a script to build a docker image

---
+++ .gitlab-ci/docker-build-image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set -u
+
+fail() { echo >&2 "$@"; exit 1; }
+
+[ $# -eq 1 ] || fail "Usage: $(basename $0) <dockerfile>"
+
+DOCKERFILE=$1
+[ -e $DOCKERFILE ] || fail "File '$DOCKERFILE' does not exist"
+
+FROM=$(grep '^FROM' $DOCKERFILE | sed 's/^FROM *//')
+[ "$FROM" ] || fail "Failed to parse dockerfile"
+
+TAG=registry.gitlab.com/goodvibes/goodvibes/$FROM
+
+docker build \
+    --tag $TAG \
+    --file $DOCKERFILE \
+    .
+
+cat << EOF
+
+--------
+
+Now you might just want to push the image:
+docker push $TAG
+
+EOF

3e99dac8509468cf68c23a3de55802da6b70b271
Arnaud Rebillout <elboulangero@gmail.com>
2018-05-12 18:17:19 +0700

    gitlab-ci: rework the build script to deal with cppflags, gcc and clang

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -3,18 +3,32 @@
 set -e
 set -u
 
-# no-deprecated-declarations is needed as long as we use GtkStatusIcon
-# no-bad-function-cast is needed for GFunc casts, typically:
-#     g_list_foreach(list, (GFunc) g_object_unref, NULL);
-export CPPFLAGS="\
+CPPFLAGS="\
  -std=gnu99 \
  -Wall \
  -Wextra \
  -Wshadow \
- -Werror \
- -Wno-deprecated-declarations \
- -Wno-bad-function-cast"
+ -Werror"
 
+# As long as we use GtkStatusIcon, we need no-deprecated-declarations
+CPPFLAGS="$CPPFLAGS -Wno-deprecated-declarations"
+
+# For some casts to GFunc, we get a warning. Typically:
+#
+#   error: cast between incompatible function types from
+#          ‘void (*)(void *)’ to ‘void (*)(void *, void *)’
+#     g_list_foreach(core_objects, (GFunc) g_object_unref, NULL);
+#                                  ^
+# gcc prefers no-cast-function-type
+# clang prefers no-bad-function-cast
+# It's unclear if an agreement will be reached.
+if [ "$CC" == "clang" ]; then
+    CPPFLAGS="$CPPFLAGS -Wno-bad-function-cast"
+else
+    CPPFLAGS="$CPPFLAGS -Wno-cast-function-type"
+fi
+
+export CPPFLAGS
 ./autogen.sh
 ./configure --enable-all
 make

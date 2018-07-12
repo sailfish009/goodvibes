ed24fda987c6a86d38b73e3fc7f0589e37f1beb0
Arnaud Rebillout <elboulangero@gmail.com>
2018-07-12 23:08:23 +0700

    meson: update goodvibes-launcher

--- goodvibes-launcher.sh
+++ goodvibes-launcher.sh
@@ -2,32 +2,27 @@
 
 # The launcher is for people who want to run Goodvibes from the source tree,
 # without installing it. GLib and GTK+ need to access some shared resources,
-# so we need to instruct them where to look for it.
+# so we need to tell them to look for it in-tree.
 
-GOODVIBES=src/goodvibes
+fail() { echo >&2 "$@"; exit 1; }
 
-fail() {
-    echo >&2 $@
-    exit 1
-}
+BUILDDIR=${BUILDDIR:-build}
+GOODVIBES=$BUILDDIR/src/goodvibes
 
-[ -x $GOODVIBES ] || fail "'$GOODVIBES' not found, did you build it?"
+[ -d $BUILDDIR ]  || fail "Build directory '$BUILDDIR' does not exit"
+[ -x $GOODVIBES ] || fail "Executable '$GOODVIBES' not found"
 
 # XDG_DATA_DIRS is needed for:
-# - GTK+ to load local icons from './data/icons'
-if [ "$XDG_DATA_DIRS" ]; then
-	if grep -F -q -v './data/:' <<< "$XDG_DATA_DIRS"; then
-		XDG_DATA_DIRS="./data/:$XDG_DATA_DIRS"
-	fi
-else
-	XDG_DATA_DIRS='./data/:/usr/local/share/:/usr/share/'
-fi
+# - GTK+ to load local icons from '$BUILDDIR/data/icons'
+[ "$XDG_DATA_DIRS" ] \
+    && XDG_DATA_DIRS="$BUILDDIR/data:$XDG_DATA_DIRS" \
+    || XDG_DATA_DIRS="$BUILDDIR/data:/usr/local/share:/usr/share"
 export XDG_DATA_DIRS
 
 # GSCHEMA_SETTINGS_DIR is needed for:
-# - GLib to load local settings schemas from './data'
+# - GLib to load local settings schemas from '$BUILDDIR/data'
 # If you don't define it, you'll be hit by something like that:
 #   [GLib-GIO] Settings schema 'com.elboulangero.Goodvibes.Core' is not installed
-export GSETTINGS_SCHEMA_DIR='./data/'
+export GSETTINGS_SCHEMA_DIR="$BUILDDIR/data/"
 
 $GOODVIBES $@

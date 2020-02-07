326a28f5280fb6bcfda47e843d0b3104842ec197
Arnaud Rebillout <elboulangero@gmail.com>
2020-02-07 23:00:14 +0700

    readthedocs: Use html_css_files to set the css font
    Signed-off-by: Arnaud Rebillout <elboulangero@gmail.com>

--- docs/goodvibes.readthedocs.io/conf.py
+++ docs/goodvibes.readthedocs.io/conf.py
@@ -86,9 +86,11 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path or fully qualified paths
+# (eg. https://...)
+html_css_files = [
+    'css/font-mfizz/font-mfizz.css',
+]
+
 # Favicon
 html_favicon = 'images/favicon.ico'
-
-# Custom css
-def setup(app):
-    app.add_stylesheet('css/font-mfizz/font-mfizz.css')

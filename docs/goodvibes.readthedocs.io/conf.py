7ae26242b706aa530944984997324e159421d1dc
Arnaud Rebillout <elboulangero@gmail.com>
2020-02-17 13:18:10 +0700

    readthedocs: Get the font mfizz from goodvibes.gitlab.io
    Signed-off-by: Arnaud Rebillout <elboulangero@gmail.com>

--- docs/goodvibes.readthedocs.io/conf.py
+++ docs/goodvibes.readthedocs.io/conf.py
@@ -90,7 +90,7 @@ html_static_path = ['_static']
 # These paths are either relative to html_static_path or fully qualified paths
 # (eg. https://...)
 html_css_files = [
-    'css/font-mfizz/font-mfizz.css',
+    'https://goodvibes.gitlab.io/fonts/font-mfizz.css',
     'css/extra.css',
 ]
 

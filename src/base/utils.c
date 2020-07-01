79e3e74ace72f8d7fc5289330d013f20193a2f60
Arnaud Rebillout <elboulangero@gmail.com>
2020-07-01 10:58:00 +0700

    Remove support for old application id
    The application id changed in version 0.4.1, released in 2018-11-10.
    According to repology [1], no distributions ship this version anymore.
    Debian stable is at 0.4.2.
    
    So I'll assume that there's no need for the migration code anymore.
    Migration happened for everyone already. Let's cleanup.
    
    [1]: https://repology.org/project/goodvibes/versions

--- src/base/utils.c
+++ src/base/utils.c
@@ -45,8 +45,8 @@ gv_in_test_suite(void)
  * Settings
  */
 
-static GSettings *
-get_current_settings(const gchar *component)
+GSettings *
+gv_get_settings(const gchar *component)
 {
 	gchar *schema_id;
 	GSettings *settings;
@@ -54,84 +54,6 @@ get_current_settings(const gchar *component)
 	schema_id = g_strdup_printf("%s.%s", GV_APPLICATION_ID, component);
 	settings = g_settings_new(schema_id);
 	g_free(schema_id);
-
-	return settings;
-}
-
-static GSettings *
-get_old_settings(const gchar *component)
-{
-	gchar *schema_id;
-	GSettings *settings;
-	GSettingsSchema *schema;
-	GSettingsSchemaSource *source;
-
-	source = g_settings_schema_source_get_default();
-	if (source == NULL)
-		return NULL;
-
-	schema_id = g_strdup_printf("%s.%s", GV_OLD_APPLICATION_ID, component);
-	schema = g_settings_schema_source_lookup(source, schema_id, TRUE);
-	g_free(schema_id);
-
-	if (schema == NULL)
-		return NULL;
-
-	settings = g_settings_new_full(schema, NULL, NULL);
-	g_settings_schema_unref(schema);
-
-	return settings;
-}
-
-static void
-merge_settings(const gchar *component, GSettings *from, GSettings *to)
-{
-	gint i;
-	gchar **keys;
-	gboolean changed;
-	GSettingsSchema *schema;
-
-	g_object_get(from, "settings-schema", &schema, NULL);
-	keys = g_settings_schema_list_keys(schema);
-	changed = FALSE;
-
-	for (i = 0; keys[i]; i++) {
-		const gchar *key;
-		GVariant *value;
-
-		key = keys[i];
-		value = g_settings_get_user_value(from, key);
-		if (value == NULL)
-			continue;
-
-		INFO("Settings migration: %s: moving key '%s'", component, key);
-		g_settings_set_value(to, key, value);
-		g_settings_reset(from, key);
-		g_variant_unref(value);
-		changed = TRUE;
-	}
-
-	if (changed) {
-		INFO("Settings migration: %s: syncing", component);
-		g_settings_sync();
-	}
-
-	g_strfreev(keys);
-	g_settings_schema_unref(schema);
-}
-
-GSettings *
-gv_get_settings(const gchar *component)
-{
-	GSettings *settings, *old_settings;
-
-	settings = get_current_settings(component);
-	old_settings = get_old_settings(component);
-
-	if (old_settings) {
-		merge_settings(component, old_settings, settings);
-		g_object_unref(old_settings);
-	}
 
 	return settings;
 }

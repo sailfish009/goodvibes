sailfish009/goodvibes

commit 054972cdbbd87bd27547dfc9a6d0f5dc81f02bcc

core/{engine,player}: Emit a signal on SSL handshake failure
Detecting that we're actually dealing with a SSL failure is a very
slippery thing. Either I got it wrong, either it's just not really
something people do.

In any case, we try hard. This has been tested with and without
translation.

The behaviour on SSL failure is that:
- the engine sends a signal, and stops the playback.
- the player catches the signal and fowards it out there.

src/core/gv-engine.c
@@ -66,6 +66,18 @@ enum {
 static GParamSpec *properties[PROP_N];
 
 /*
+ * Signals
+ */
+
+enum {
+        SIGNAL_SSL_FAILURE,
+        /* Number of signals */
+        SIGNAL_N
+};
+
+static guint signals[SIGNAL_N];
+
+/*
  * GObject definitions
  */
 
@@ -733,16 +745,34 @@ on_bus_message_error(GstBus *bus G_GNUC_UNUSED, GstMessage *msg, GvEngine *self)
 	        g_quark_to_string(err->domain), err->code, err->message);
 	WARNING("Gst bus error debug: %s", debug);
 
+	/* Stop playback otherwise gst keeps on spitting errors */
+	set_gst_state(priv->playbin, GST_STATE_NULL);
+
+	/* And now, some effort to handle the error.
+	 * Note that we attempt to match a translated string. There is absolutely
+	 * no guarantee that this string won't change over time. This is very bad
+	 * and it will likely break and go unnoticed in some not so far future.
+	 * If you have a better solution, I'd like to hear about it.
+	 */
+	if (g_error_matches(err, GST_RESOURCE_ERROR, GST_RESOURCE_ERROR_OPEN_READ) &&
+	    !g_strcmp0(err->message, g_dgettext("gst-plugins-good-1.0", "Secure connection setup failed."))) {
+		/* SSL failure, no point retrying, just signal then. We drop the
+		 * first line of the debug message as we know that it's about
+		 * GST internals.
+		 */
+		const char *ptr;
+		ptr = strchr(debug, '\n');
+		ptr = ptr ? ptr + 1 : debug;
+		g_signal_emit(self, signals[SIGNAL_SSL_FAILURE], 0, err->message, ptr);
+	} else {
+		/* When in doubt, retry! */
+		if (self->priv->state != GV_ENGINE_STATE_STOPPED)
+			retry_playback(self);
+	}
+
 	/* Cleanup */
 	g_error_free(err);
 	g_free(debug);
-
-	/* Stop immediately otherwise gst keeps on spitting errors */
-	set_gst_state(priv->playbin, GST_STATE_NULL);
-
-	/* Restart playback on error */
-	if (self->priv->state != GV_ENGINE_STATE_STOPPED)
-		retry_playback(self);
 
 	/* Emit an error signal */
 	//gv_errorable_emit_error(GV_ERRORABLE(self), "GStreamer error: %s", err->message);
@@ -1219,4 +1249,10 @@ gv_engine_class_init(GvEngineClass *class)
 	                            GV_PARAM_READWRITE);
 
 	g_object_class_install_properties(object_class, PROP_N, properties);
+
+	/* Signals */
+	signals[SIGNAL_SSL_FAILURE] =
+	        g_signal_new("ssl-failure", G_TYPE_FROM_CLASS(class),
+	                     G_SIGNAL_RUN_LAST, 0, NULL, NULL, NULL,
+			     G_TYPE_NONE, 2, G_TYPE_STRING, G_TYPE_STRING);
 }

src/core/gv-player.c
@@ -75,6 +75,18 @@ enum {
 static GParamSpec *properties[PROP_N];
 
 /*
+ * Signals
+ */
+
+enum {
+        SIGNAL_SSL_FAILURE,
+        /* Number of signals */
+        SIGNAL_N
+};
+
+static guint signals[SIGNAL_N];
+
+/*
  * GObject definitions
  */
 
@@ -214,6 +226,16 @@ on_engine_error(GvEngine *engine G_GNUC_UNUSED,
 	gv_player_stop(self);
 }
 
+static void
+on_engine_ssl_failure(GvEngine *engine G_GNUC_UNUSED,
+                      const gchar *error,
+		      const gchar *debug,
+		      GvPlayer *self)
+{
+	/* Just forward the signal ... */
+	g_signal_emit(self, signals[SIGNAL_SSL_FAILURE], 0, error, debug);
+}
+
 /*
  * Property accessors - construct-only properties
  */
@@ -231,6 +253,7 @@ gv_player_set_engine(GvPlayer *self, GvEngine *engine)
 	/* Some signal handlers */
 	g_signal_connect_object(engine, "notify", G_CALLBACK(on_engine_notify), self, 0);
 	g_signal_connect_object(engine, "error", G_CALLBACK(on_engine_error), self, 0);
+	g_signal_connect_object(engine, "ssl-failure", G_CALLBACK(on_engine_ssl_failure), self, 0);
 }
 
 static void
@@ -1024,4 +1047,10 @@ gv_player_class_init(GvPlayerClass *class)
 	                            GV_PARAM_READABLE);
 
 	g_object_class_install_properties(object_class, PROP_N, properties);
+
+	/* Signals */
+	signals[SIGNAL_SSL_FAILURE] =
+	        g_signal_new("ssl-failure", G_TYPE_FROM_CLASS(class),
+	                     G_SIGNAL_RUN_LAST, 0, NULL, NULL, NULL,
+			     G_TYPE_NONE, 2, G_TYPE_STRING, G_TYPE_STRING);
 }

commit b75d8e6d86a61f493b9237f3f392ba575e6dd15c

core/engine: Fix the code that detects SSL failures
Fix sloppy code introduced in 054972c.

Thx Stephane Cerveau for showing the way.

src/core/gv-engine.c
@@ -23,6 +23,7 @@
 #include <glib-object.h>
 #include <gst/gst.h>
 #include <gst/audio/streamvolume.h>
+#include <libsoup/soup.h>
 
 #include "base/glib-object-additions.h"
 #include "base/gv-base.h"
@@ -748,22 +749,18 @@ on_bus_message_error(GstBus *bus G_GNUC_UNUSED, GstMessage *msg, GvEngine *self)
 	/* Stop playback otherwise gst keeps on spitting errors */
 	set_gst_state(priv->playbin, GST_STATE_NULL);
 
-	/* And now, some effort to handle the error.
-	 * Note that we attempt to match a translated string. There is absolutely
-	 * no guarantee that this string won't change over time. This is very bad
-	 * and it will likely break and go unnoticed in some not so far future.
-	 * If you have a better solution, I'd like to hear about it.
+	/* Here comes the actual effort to handle errors. At the moment there's
+	 * not much to it, we only handle SSL failures.
 	 */
-	if (g_error_matches(err, GST_RESOURCE_ERROR, GST_RESOURCE_ERROR_OPEN_READ) &&
-	    !g_strcmp0(err->message, g_dgettext("gst-plugins-good-1.0", "Secure connection setup failed."))) {
-		/* SSL failure, no point retrying, just signal then. We drop the
-		 * first line of the debug message as we know that it's about
-		 * GST internals.
-		 */
-		const char *ptr;
-		ptr = strchr(debug, '\n');
-		ptr = ptr ? ptr + 1 : debug;
-		g_signal_emit(self, signals[SIGNAL_SSL_FAILURE], 0, err->message, ptr);
+	if (g_error_matches(err, GST_RESOURCE_ERROR, GST_RESOURCE_ERROR_OPEN_READ)) {
+		const GstStructure *details = NULL;
+		gst_message_parse_error_details(msg, &details);
+		if (details && gst_structure_has_field_typed(details, "http-status-code", G_TYPE_UINT)) {
+			guint code = 0;
+			gst_structure_get_uint(details, "http-status-code", &code);
+			if (code == SOUP_STATUS_SSL_FAILED)
+				g_signal_emit(self, signals[SIGNAL_SSL_FAILURE], 0, err->message, debug);
+		}
 	} else {
 		/* When in doubt, retry! */
 		if (self->priv->state != GV_ENGINE_STATE_STOPPED)
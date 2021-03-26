30c8f6d936f7eb330e98505d359acb2571273f3c
Jeroen Baten <jbaten@i2rs.nl>
2021-03-26 09:33:19 +0000

    Update src/default-stations.h

--- src/default-stations.h
+++ src/default-stations.h
@@ -107,6 +107,166 @@
         "  <uri>http://live.radiogrenouille.com/live</uri>" \
         "</Station>"
 
+
+/*
+ * SomaFM Station list
+ * See https://somafm.com
+ */
+
+#define DEFAULT_STATIONS_SOMAFM \
+        "<Station>" \
+        "  <name>SomaFM Groove Salad</name>" \
+        "  <uri>https://somafm.com/groovesalad130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Drone Zone</name>" \
+        "  <uri>https://somafm.com/dronezone130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Indie Pop</name>" \
+        "  <uri>https://somafm.com/indiepop130.pls</uri>" \>
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Deep Space One</name>" \
+        "  <uri>https://somafm.com/deepspaceone130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Space Station</name>" \
+        "  <uri>https://somafm.com/spacestation130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Lush</name>" \
+        "  <uri>https://somafm.com/lush130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Secret Agent</name>" \
+        "  <uri>https://somafm.com/secretagent130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Underground 80s</name>" \
+        "  <uri>https://somafm.com/u80s130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM0 Groove Salad Classic</name>" \
+        "  <uri>https://somafm.com/gsclassic130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Seventies</name>" \
+        "  <uri>https://somafm.com/seventies130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Folk</name>" \
+        "  <uri>https://somafm.com/folkfwd130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM PopTron</name>" \
+        "  <uri>https://somafm.com/poptron130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Beat Blender</name>" \
+        "  <uri>https://somafm.com/beatblender130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Boot Liquor</name>" \
+        "  <uri>https://somafm.com/bootliquor130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM DEF CON Radio</name>" \
+        "  <uri>https://somafm.com/defcon130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM The Trip</name>" \
+        "  <uri>https://somafm.com/thetrip130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM BAGeL Radio</name>" \
+        "  <uri>https://somafm.com/bagel130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Synphaera</name>" \
+        "  <uri>https://somafm.com/synphaera130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Suburbs of Goa</name>" \
+        "  <uri>https://somafm.com/suburbsofgoa130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Fluid</name>" \
+        "  <uri>https://somafm.com/fluid130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Sonic Universe</name>" \
+        "  <uri>https://somafm.com/sonicuniverse130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Mission Control</name>" \
+        "  <uri>https://somafm.com/missioncontrol130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Seven Inch Soul</name>" \
+        "  <uri>https://somafm.com/7soul130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM iIllinois Street Lounge</name>" \
+        "  <uri>https://somafm.com/illstreet130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Dub Step Beyond</name>" \
+        "  <uri>https://somafm.com/dubstep130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM iThistleRadio</name>" \
+        "  <uri>https://somafm.com/thistle130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM ckiqhop idm</name>" \
+        "  <uri>https://somafm.com/cliqhop130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM iHeavyweight Reggae</name>" \
+        "  <uri>https://somafm.com/reggae130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Metal Detector</name>" \
+        "  <uri>https://somafm.com/metal130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Digitalis</name>" \
+        "  <uri>https://somafm.com/digitalis130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM n5MD Radio</name>" \
+        "  <uri>https://somafm.com/n5md130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Vaporwaves</name>" \
+        "  <uri>https://somafm.com/vaporwaves130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM SF 10-33</name>" \
+        "  <uri>https://somafm.com/sf1033130.pls</uri>" \https://somafm.com
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Black Rock FM</name>" \
+        "  <uri>https://somafm.com/brfm130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Covers</name>" \
+        "  <uri>https://somafm.com/covers130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Live</name>" \
+        "  <uri>https://somafm.com/live130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM Specials</name>" \
+        "  <uri>https://somafm.com/specials130.pls</uri>" \
+        "</Station>" \
+        "<Station>" \
+        "  <name>SomaFM SF Police Scanner</name>" \
+        "  <uri>https://somafm.com/scanner130.pls</uri>" \
+        "</Station>"
+
 /*
  * Default stations
  */
@@ -116,4 +276,5 @@
         DEFAULT_STATIONS_FIP \
 	DEFAULT_STATIONS_NOVA \
         DEFAULT_STATIONS_MISC \
+	DEFAULT_STATIONS_SOMAFM \
         "</Stations>"

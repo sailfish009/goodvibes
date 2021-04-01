cecefcc2705ffcc470d8e8990f92443313e1c1b9
Arnaud Rebillout <elboulangero@gmail.com>
2021-04-01 10:51:20 +0700

    Relicense to GPL v3.0 only (was GPL v3.0 or later)

--- scripts/code/standard-header.sh
+++ scripts/code/standard-header.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-SPDX_LICENSE_ID=GPL-3.0-or-later
+SPDX_LICENSE_ID=GPL-3.0-only
 
 source $(dirname $0)/lib-git.sh
 
@@ -46,8 +46,7 @@ do_add()
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- src/core/tests/station-list.c
+++ src/core/tests/station-list.c
@@ -3,12 +3,11 @@
  *
  * Copyright (C) 2019-2021 Arnaud Rebillout
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: GPL-3.0-only
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of

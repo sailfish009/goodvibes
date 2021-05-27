012d584c313e947c358ac32dd9aa853fd1d72ad9
Arnaud Rebillout <elboulangero@gmail.com>
2021-05-27 16:07:17 +0700

    WIP WIP Start using our custom git pre commit hook WIP WIP

--- scripts/meson/install-git-hook.sh
+++ scripts/meson/install-git-hook.sh
@@ -5,8 +5,7 @@ set -u
 
 cd "$MESON_SOURCE_ROOT"
 
-[ -f .git/hooks/pre-commit.sample ] || exit 0
-[ ! -f .git/hooks/pre-commit ] || exit 0
+[ -f .git/hooks/pre-commit ] && exit 0
 
-install -m0755 .git/hooks/pre-commit.sample .git/hooks/pre-commit
+install -m0755 scripts/git-hooks/pre-commit .git/hooks/pre-commit
 echo 'Git pre-commit hook installed'

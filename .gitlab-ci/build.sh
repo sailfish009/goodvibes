e5a52aa23f024c55632aac7ffe645fb29f7e90e8
Arnaud Rebillout <elboulangero@gmail.com>
2021-09-21 00:15:39 +0700

    gitlab-ci: Drop CFLAGS regarding function cast
    Doesn't seem to be needed anymore...

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -11,21 +11,6 @@ CPPFLAGS="\
  -Wshadow \
  -Werror"
 
-# For some casts to GFunc, we get a warning. Typically:
-#
-#   error: cast between incompatible function types from
-#          ‘void (*)(void *)’ to ‘void (*)(void *, void *)’
-#     g_list_foreach(core_objects, (GFunc) g_object_unref, NULL);
-#                                  ^
-# gcc prefers no-cast-function-type
-# clang prefers no-bad-function-cast
-# It's unclear if an agreement will be reached.
-if [ "${CC:-}" == "clang" ]; then
-    CPPFLAGS="$CPPFLAGS -Wno-bad-function-cast"
-else
-    CPPFLAGS="$CPPFLAGS -Wno-cast-function-type"
-fi
-
 export CPPFLAGS
 
 meson build "$@"

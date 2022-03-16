469fb5f394c8a2f20eb762af43d11a4c65bd7fc1
Arnaud Rebillout <elboulangero@gmail.com>
2022-03-16 21:45:55 +0700

    scripts: Add some comments to clarify install-git-hook

--- scripts/meson/install-git-hook.sh
+++ scripts/meson/install-git-hook.sh
@@ -5,8 +5,9 @@ set -u
 
 cd "$MESON_SOURCE_ROOT"
 
-[ -f .git/hooks/pre-commit.sample ] || exit 0
-[ ! -f .git/hooks/pre-commit ] || exit 0
+[ -d .git ] || exit 0 # not a git repo
+[ -f .git/hooks/pre-commit.sample ] || exit 0 # doesn't hurt to check
+[ ! -f .git/hooks/pre-commit ] || exit 0 # already installed
 
-install -m0755 .git/hooks/pre-commit.sample .git/hooks/pre-commit
+install -m 0755 .git/hooks/pre-commit.sample .git/hooks/pre-commit
 echo 'Git pre-commit hook installed'
